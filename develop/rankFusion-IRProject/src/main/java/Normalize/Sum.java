8ad0ff05e97efd44eeb8c8a831a4d419368f3610
Davide Rigoni <davider1994@gmail.com>
2017-12-09 16:23:26 +0100

    Add structure of Standard and Sum normalization

---
+++ develop/rankFusion-IRProject/src/main/java/Normalize/Sum.java
@@ -0,0 +1,17 @@
+package Normalize;
+
+import RunObject.RunList;
+
+/**
+ * Author: davide
+ * Github Name: drigoni
+ * Date: 09/12/17
+ *
+ * This class represent the sum normalization where
+ * shift min to 0 and sum to 1
+ */
+public class Sum extends  AbsNormalize{
+    public RunList execute(RunList listOfRun) {
+        return null;
+    }
+}
