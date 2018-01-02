566d71d4f3b544d9c65e8a7da153e1f55172a28b
abeccaro <alex.becks@hotmail.it>
2018-01-02 16:53:35 +0100

    Updated PrbFuse algorithm with correct segments

--- develop/rankFusion-IRProject/src/main/java/RankFusion/ProbFuse.java
+++ develop/rankFusion-IRProject/src/main/java/RankFusion/ProbFuse.java
@@ -12,6 +12,8 @@ import java.util.*;
  */
 public class ProbFuse extends AbsRankFusion {
 
+    private static final int SEGMENT_SIZE = 20;
+
     public Run Fuse(RunList runList, AssessmentList assessmentList) {
         List<Map<Run.Key, Double>> probabilities =
                 new ArrayList<Map<Run.Key, Double>>();
@@ -36,33 +38,92 @@ public class ProbFuse extends AbsRankFusion {
      */
     private Map<Run.Key, Double> calculateRunProbabilities
     (Run run, AssessmentList assessmentList) {
+        System.out.println("Calculating run " + run.getName());
+
         Map<Run.Key, Double> probabilities = new HashMap<Run.Key, Double>();
 
-        // getting number of relevant and total documents for each topic
-        Map<String, Integer> relevantDocsCount = new HashMap<String, Integer>();
-        Map<String, Integer> docsCount = new HashMap<String, Integer>();
-        for (AssessmentElement element : assessmentList) {
-            String topic = element.getTopic();
-            docsCount.put(topic, (docsCount.containsKey(topic) ?
-                    docsCount.get(topic) : 0) + 1);
+        Map<String, List<RunElement>> elementsByTopic = new HashMap<String, List<RunElement>>();
 
-            if (element.getAssessment())
-                relevantDocsCount.put(topic,
-                        (relevantDocsCount.containsKey(topic)
-                                ? relevantDocsCount.get(topic) : 0) + 1);
+        for (RunElement e : run) {
+            String topic = e.getTopic();
+            if (elementsByTopic.containsKey(topic))
+                elementsByTopic.get(topic).add(e);
+            else {
+                List<RunElement> list = new ArrayList<RunElement>();
+                list.add(e);
+                elementsByTopic.put(topic, list);
+            }
         }
 
-        // calculating probabilities
-        for (RunElement element : run) {
-            String topic = element.getTopic();
-            double probability = (double) relevantDocsCount.get(topic)
-                    / docsCount.get(topic);
+        int relevant, nonRelevant, segment;
+        List<RunElement> segmentElements = new ArrayList<RunElement>();
 
-            Run.Key key = new Run.Key(topic, element.getDocument());
-            probabilities.put(key, probability);
+        for (String topic: elementsByTopic.keySet()) {
+            List<RunElement> topicElements = elementsByTopic.get(topic);
+
+            segmentElements.clear();
+            relevant = 0;
+            nonRelevant = 0;
+            segment = 1;
+
+            for (RunElement element : topicElements) {
+                // if relevant increment relative counter
+                AssessmentElement ae = assessmentList.get(element.getDocument(), element.getTopic());
+                if (ae != null) {
+                    if (ae.getAssessment())
+                        relevant++;
+                    else
+                        nonRelevant++;
+                }
+
+                // adding element to segment
+                segmentElements.add(element);
+
+                // if current segment is full calculate probabilities and clear segment data
+                if (segmentElements.size() == SEGMENT_SIZE) {
+                    double probability = ((double) relevant / (relevant + nonRelevant)) / segment;
+                    addSegmentProbabilities(probabilities, segmentElements, probability);
+
+                    segmentElements.clear();
+                    relevant = 0;
+                    nonRelevant = 0;
+
+                    segment++;
+                }
+            }
+
+            // calculate probabilities of last segment if not complete
+            if (!segmentElements.isEmpty()) {
+                double probability = ((double) relevant / (relevant + nonRelevant)) / segment;
+                addSegmentProbabilities(probabilities, segmentElements, probability);
+            }
         }
+
+        System.out.println("Finished calculating run " + run.getName());
 
         return probabilities;
+    }
+
+    /**
+     * Adds the partial probabilities to already calculated ones of each document in a segment
+     * @param probabilities The already calculated probabilities
+     * @param segmentElements The elements in the segment
+     * @param probability The probability to add to each element in the segment
+     */
+    private void addSegmentProbabilities(Map<Run.Key, Double> probabilities,
+                                               List<RunElement> segmentElements,
+                                               double probability) {
+        for (RunElement elem : segmentElements) {
+            Run.Key key = new Run.Key(elem.getTopic(), elem.getDocument());
+
+            // if key already present
+            if (probabilities.containsKey(key))
+                // add probability to old value
+                probabilities.put(key, probabilities.get(key) + probability);
+            else
+                // else set it to probability
+                probabilities.put(key, probability);
+        }
     }
 
     /**
@@ -81,11 +142,13 @@ public class ProbFuse extends AbsRankFusion {
         int rank = 0;
         for (String topic : topics) {
             for (String document : documents) {
+                //System.out.println("Topic: " + topic + " - document: " + document);
                 double score = 0;
 
                 for (Map<Run.Key, Double> probs : probabilities) {
                     Run.Key key = new Run.Key(topic, document);
                     score += probs.containsKey(key) ? probs.get(key) : 0d;
+                    //System.out.println("Score updated to: " + score);
                 }
 
                 // assuming 1 query
